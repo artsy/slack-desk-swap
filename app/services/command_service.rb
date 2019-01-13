16035a64dbc37cbe3fad0f3435390f4dbb01e007
Ashkan Nasseri <ashkan@artsymail.com>
2019-01-13 14:41:31 -0500

    Add some test

--- app/services/command_service.rb
+++ app/services/command_service.rb
@@ -12,8 +12,8 @@ module CommandService
   end
 
   def self.join(user_id, user_name, team_id)
-    team = Team.find_or_create!(team_id: team_id)
-    user = team.users.find_or_create!(user_id: user_id, user_name: user_name)
+    team = Team.find_or_create_by!(team_id: team_id)
+    user = team.users.find_or_create_by!(user_id: user_id, user_name: user_name)
     "Awesome! You are setup for desk-sawp! wait for our message to setup your first 🖥 🔄"
   end
 end
