16fbd7d93588731a4e4cd3051578c0d9bd54e0ef
ashkan <ashkan@artsymail.com>
2019-01-18 09:29:30 -0500

    Fix migration version

--- db/migrate/20190114011003_create_rounds.rb
+++ db/migrate/20190114011003_create_rounds.rb
@@ -1,4 +1,4 @@
-class CreateRounds < ActiveRecord::Migration[5.1]
+class CreateRounds < ActiveRecord::Migration[5.0]
   def change
     create_table :rounds do |t|
       t.references :team, foreign_key: true
