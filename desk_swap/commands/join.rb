08035d0e7d765a82bec85d717b161af041936176
ashkan18 <ashkan18@gmail.com>
2019-01-19 17:55:23 -0500

    Move log message to the top of command

--- desk_swap/commands/join.rb
+++ desk_swap/commands/join.rb
@@ -1,10 +1,10 @@
 class Join < SlackRubyBot::Commands::Base
   def self.call(client, data, _match)
+    logger.info "Join: #{client.owner.class}, user=#{data.user}"
     user = CommandService.join(data.user, client.owner.id) do |user|
       # new user
       client.say(channel: data.channel, text: 'Welcome to desk swap!')
       client.say(channel: data.channel, text: 'Please set your location and tell us about your current setup!')
     end
-    logger.info "UNAME: #{client.owner}, user=#{data.user}"
   end
 end
--- desk_swap/services/command_service.rb
+++ desk_swap/services/command_service.rb
@@ -1,6 +1,6 @@
 module CommandService
   def self.join(user_id, team_id, &block)
-    team = Team.find_by(team_id: team_id)
+    team = Team.find(team_id)
     team.users.find_or_create_by!(user_id: user_id, &block)
   end
 end
