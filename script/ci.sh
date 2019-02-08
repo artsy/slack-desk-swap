aecb46c6320a1398de86e842a0bc50133fb28570
ashkan <ashkan@artsymail.com>
2019-02-08 09:20:52 -0500

    Wait for it, wait for database

--- script/ci.sh
+++ script/ci.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+./script/wait-for-it.sh slack-deskswap-db:5432 -- echo "Database is READY... phew...."
 rake db:create RACK_ENV=test
 rake db:setup RACK_ENV=test
 rake db:migrate RACK_ENV=test
